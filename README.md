611ec3990dca4314baa3c74265821e18a4302cd8
Hemish Pancholi <hemishpancholi11@gmail.com>
2020-10-17 13:16:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# carbon-12
+ An open source Periodic Table app created by me with neumorphic design!!
---
+++ android/app/src/main/kotlin/com/hemish/carbon12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hemish.carbon12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
